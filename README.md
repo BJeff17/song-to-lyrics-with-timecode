43cb99764c1b1648c9c6bb66ee570d20e564f85a
BJeff17 <jeffvieyra2007@gmail.com>
2026-01-17 21:29:55 +0100

    testing pydub integration before a complete setup for the transcription

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # song-to-lyrics-with-timecode
-I think I'm gonna use pydub.
+I think I'm gonna use pydub split on silence functionality to deliver a really qualitative transcription with accurate timestamp.
--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 from transformers import pipeline, AutoModelForSpeechSeq2Seq, AutoProcessor
 import torch
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
-model_id = "openai/whisper-small"
-cache_dir = "./models/whisper-small"
+model_id = "openai/whisper-tiny"
+cache_dir = "./models/whisper-tiny"
+
+import numpy as np
+
+def audiosegment_to_numpy(audio):
+    samples = np.array(audio.get_array_of_samples())
+
+    # Convert stereo → mono if needed
+    if audio.channels > 1:
+        samples = samples.reshape((-1, audio.channels))
+        samples = samples.mean(axis=1)
+
+    return samples.astype(np.float32), audio.frame_rate
+
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id,
@@ -24,5 +39,8 @@ pipe = pipeline(
     return_timestamps=True
 )
 
-result = pipe("Alec Benjamin - Let Me Down Slowly (Lyrics)(MP3_160K).mp3")
+audio = AudioSegment.from_file("Alec Benjamin - Let Me Down Slowly (Lyrics)(MP3_160K).mp3")
+audio_segments = split_on_silence(audio, min_silence_len=700, silence_thresh=-40)
+audio_segments_np = [audiosegment_to_numpy(segment)[0] for segment in audio_segments]
+result = pipe(audio_segments_np[0])
 print(result)
